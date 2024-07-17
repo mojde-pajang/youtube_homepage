ad7dea5d29e6cb609ae6e639cfed4fd35d3299d4
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-07-17 09:43:27 +0200

    fix sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { categories, videos } from "./lib/data"
 function App() {
   const [activeCategory, setActiveCategory] = useState(categories[0])
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='max-h-screen flex flex-col'>
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] gap-7 flex-grow-1 overflow-auto">
         <div>Sidebar</div>
